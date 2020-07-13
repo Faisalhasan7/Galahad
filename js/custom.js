40a1fa8af6560d3f946a48607c47632fb2b80d5e
Faisalhasan7 <64736773+Faisalhasan7@users.noreply.github.com>
2020-07-13 20:08:02 +0600

    v1.0

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(function () {
-
+    // 1. jQuery Owl Carousel
     $(document).ready(function () {
         $(".owl-carousel").owlCarousel();
     });
@@ -8,41 +8,102 @@ $(function () {
     owl.owlCarousel({
         items: 1,
         loop: true,
-        margin: 10,
+        margin: 0,
         autoplay: true,
         autoplayTimeout: 2000,
+        fluidSpeed: 1500,
         autoplayHoverPause: true
     });
-    $('.play').on('click', function () {
-        owl.trigger('play.owl.autoplay', [1000])
-    })
-    $('.stop').on('click', function () {
-        owl.trigger('stop.owl.autoplay')
-    })
-    
-    //  2. easeScroll
-    $("html").easeScroll();
-    
-    var scroll = new SmoothScroll('a[href*="#"]');
 
-    //jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $(document).on('click', 'a.page-scroll', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top-75            }, 900, 'easeInOutExpo');
-            event.preventDefault();
+    // 2. Venobox Initialize plugin
+    $(document).ready(function () {
+        $('.venobox').venobox();
+    });
+
+    // 3. Animation scroll js
+    var html_body = $('html, body');
+    $('.navbar a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 50
+                }, 1500, );
+                return false;
+            }
+        }
+    });
+
+    // 4. CounterUp Initialize
+    jQuery(document).ready(function ($) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
         });
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    // 5. jQuery Slick Slider
+    $('.slick').slick({
+        infinite: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        speed: 1500,
+        nextArrow: '.fa-angle-right',
+        prevArrow: '.fa-angle-left',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });
+
+    //6. Back to TOp    
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500);
+    });
+
+    //7. Back top button fadein fadeout / menu add class  
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+        if (scrolling > 200) {
+            $('.back-to-top').fadeIn(500);
+        } else {
+            $('.back-to-top').fadeOut(500);
+        }
+
+        if (scrolling > 150) {
+            $('.navbar').addClass('navauto');
+        } else {
+            $('.navbar').removeClass('navauto');
+        }
+    });
 });
